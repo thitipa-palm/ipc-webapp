4f3efd75138e3f462e216d9a6013391589c10ec0
RAEVIRE <momae.w@outlook.com>
2021-11-07 14:33:55 +0700

    m3
    add recomend strech info

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,4 +1,10 @@
+//import pic - there should be more effective way to do this but...
 import sit_img from 'assets/img/ipc/sit.png';
+import strech1_img from 'assets/img/ipc/strech1.jpg';
+import strech2_img from 'assets/img/ipc/strech2.jpg';
+import strech3_img from 'assets/img/ipc/strech3.jpg';
+import strech4_img from 'assets/img/ipc/strech4.jpg';
+
 import { AnnouncementCard, TodosCard } from 'components/Card';
 import HorizontalAvatarList from 'components/HorizontalAvatarList';
 import MapWithBubbles from 'components/MapWithBubbles';
@@ -9,7 +15,6 @@ import UserProgressTable from 'components/UserProgressTable';
 import { IconWidget, NumberWidget } from 'components/Widget';
 import { getStackLineChart, stackLineChartOptions } from 'demos/chartjs';
 import {
-  
   avatarsData,
   chartjs,
   productsData,
@@ -66,6 +71,7 @@ dbRef.child("sensor").get().then((snapshot) => {
   console.error(error);
 });
 
+//Edit Todo list Data here//
 const ipctodosData = [
   { id: 1, title: 'Stand up' },
   { id: 2, title: 'Upper-body Stretch'},
@@ -217,7 +223,7 @@ class DashboardPage extends React.Component {
         <Row>
         <Col xl={7} lg={12} md={12}>
           <Card>
-            <CardHeader>Today Summary</CardHeader>
+            <CardHeader><h5><strong>Today Summary</strong></h5></CardHeader>
             <CardBody>
               <Pie data={genIpcPieData()} />
             </CardBody>
@@ -287,28 +293,64 @@ class DashboardPage extends React.Component {
           <Card className="flex-row">
             <CardImg
               className="card-img-left"
-              src={sit_img}
+              src={strech2_img}
               style={{ width: 'auto', height: 150 }}
             />
             <CardBody>
-              <CardTitle>Horizontal Image Card(Left)</CardTitle>
+              <CardTitle><h7><strong>Hamstrings</strong></h7></CardTitle>
               <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              Stand split stance and put both of your hands on the knee in front
+               of you and keep the other knee straight, push your hip forwards and down 
+               keeping your back straight.Place the hands on the top of the hip for support. 
+               Hold the stretch for 30 seconds and then switch to other side and repeat the steps.
               </CardText>
             </CardBody>
           </Card>
           <Card className="flex-row">
             <CardImg
               className="card-img-left"
-              src={sit_img}
+              src={strech1_img}
               style={{ width: 'auto', height: 150 }}
             />
             <CardBody>
-              <CardTitle>Horizontal Image Card(Left)</CardTitle>
+              <CardTitle><h7><strong>Upper Back Stretch</strong></h7></CardTitle>
               <CardText>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+              interlock your fingers behind your back, then lift your arms so you feel a 
+              stretch in your chest and front shoulders. Draw your chin down to avoid crunching the neck.
+              </CardText>
+            </CardBody>
+          </Card>
+          <Card className="flex-row">
+            <CardImg
+              className="card-img-left"
+              src={strech3_img}
+              style={{ width: 'auto', height: 150 }}
+            />
+            <CardBody>
+              <CardTitle><h7><strong>Wrists and forearm</strong></h7></CardTitle>
+              <CardText>
+              Perform a prayer stretch (Buddha stretch) by placing your fingers and palms 
+              together with your hands in front of your chest and fingers pointing upward. 
+              While keeping your palms together and having your elbows away from the body, 
+              you should slowly lower your hands until you feel a good stretch around your wrists. 
+              Hold for five seconds.
+              </CardText>
+            </CardBody>
+          </Card>
+          <Card className="flex-row">
+            <CardImg
+              className="card-img-left"
+              src={strech4_img}
+              style={{ width: 'auto', height: 150 }}
+            />
+            <CardBody>
+              <CardTitle><h7><strong>Hip Flexors</strong></h7></CardTitle>
+              <CardText>
+              Stand tall with back straight, abs engaged, shoulders down, and feet hip-width apart. 
+              Bring your left leg forward, heel down, toes up and leg straight. Keeping back straight 
+              and abs engaged, bend the right knee as if sitting back, while supporting yourself with 
+              both hands on your thighs. Breathe deeply and hold for 10-30 seconds. Switch legs and repeat 
+              on the other side.
               </CardText>
             </CardBody>
           </Card>
